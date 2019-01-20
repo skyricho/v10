0420ba5b9424e8d14afef0b2db74264789ba0a6a
Sky Richardson <skyricho@gmail.com>
2019-01-20 16:53:19 +1100

    Update mapDetails.php
    Add more stats to  map coverage card

--- mapDetails.php
+++ mapDetails.php
@@ -1,5 +1,6 @@
 <?php
 include ("dbaccess.php"); 
+ini_set('display_errors', 1);
 
 $request = $fm->newFindCommand('Map');
 $request->addFindCriterion('Map', $_GET['Map'] ); 
@@ -9,23 +10,72 @@ $records = $result->getRecords();
 # get first record ID to populate nav pull downs
 foreach($records as $record) {
     $Coverage = $record->getField('coverage');
-    $Contacted = $record->getField('cAH');
-    $Addresses = $record->getField('cAH') + $record->getField('cNH');
+    $SingleDwelling = $record->getField('cSingleDwellingCount');
+    $SingleDwellingAH = $record->getField('cSingleDwellingAH');
+    $MultiDwelling = $record->getField('cMultiDwellingCount');
+    $MultiDwellingAH = $record->getField('cMultiDwellingCountAH');
+    $DNC = $record->getField('cDNCcount');
+    $Contacted = $record->getField('contactedTotal') - $record->getField('MapAssignment::startContacted');//deprecate
+    $Addresses = $record->getField('nhTotal');//deprecate
     $assignedTo = $record->getField('MapAssignment::cFirstName') . ' ' . $record->getField('User::Last');
     $mobile = str_replace(" ","",$record->getField('User::Mobile'));
     $MapAssignmentId = $record->getField('mapAssignmentId');
   break; // break loop after first iteration
 }
 
-echo '<p>Coverage <span class="badge badge-primary">' . $Coverage . '%</span> (' . $Contacted . ' of ' . $Addresses . ' addresses)<br>Map assigned to ' . '<a href="sms:' . $mobile . '">' . $assignedTo . '</p></a>';
+
+
+echo '<div class="card my-3">
+        <div class="card-body">
+        <h5>Coverage <span class="badge badge-primary">' . $Coverage . '%</span></h6>
+          <ul class="list-unstyled">
+            <li>Houses: ' . $SingleDwellingAH . ' of ' . $SingleDwelling . '</li>
+            <li>Units: ' . $MultiDwellingAH . ' of ' . $MultiDwelling . '</li>';
+
+if ( $DNC > 0) {
+  echo '<li>DNCs: ' . $DNC . '</li>';
+}
+echo     '</ul>
+          <p>Map assigned to <a href="sms:' . $mobile . '">' . $assignedTo . '</a></p>
+        </div>
+      </div>'; 
 ?>
 
-  <div id="alert"></div>  
+<?php
+# Query available blocks
+$request = $fm->newFindCommand('updateMapBlock');
+$request->addFindCriterion('Map', $_GET['Map']); 
+$result = $request->execute();
+$records = $result->getRecords();
 
-  <form ic-post-to="admin/map.php" ic-confirm="Are you sure?" ic-target="#alert">
+echo '<div class="card my-3">
+        <div class="card-body">
+          <div id="response"></div>';
+foreach($records as $record) {  
+    echo '<div class="form-check">';
+    if ($record->getField('isDone') === 'yes') {
+      //echo $record->getField('isDone');
+      //echo '<input id="checkbox-' . $record->getField('recID') . '" type="checkbox" name="isDone" value="no" checked>';
+      echo '<input type="checkbox" class="form-check-input" ic-post-to="block-is-done.php?recID=' . $record->getField('recID') . '" ic-target="#response"  name="isDone" value="no" checked>';
+    } else {
+      //echo 'qux';
+      echo '<input type="checkbox" class="form-check-input" ic-post-to="block-is-done.php?recID=' . $record->getField('recID') . '" ic-target="#response"  name="isDone" value="yes">';
+    }
+      echo '<label class="form-check-label">Block ' . $record->getField('Block') . '</label>
+        </div>';
+           //<input class="btn btn-outline-primary btn-block" type="submit" name="foo" value="bar">
+          //echo '</form>';
+}   
+echo '</div>
+    </div>'; 
+?>
+
+<div id="alert"></div> 
+
+<form ic-post-to="admin/map.php" ic-confirm="Are you sure?" ic-target="#alert">
     <!--<form action="admin/map.php" method="POST">-->
 
-    <input type="hidden" name="id" value="<?php echo $MapAssignmentId; ?>">
+   <input type="hidden" name="id" value="<?php echo $MapAssignmentId; ?>">
     <input type="hidden" name="cAH" value="<?php echo $Contacted; ?>">
     <input type="hidden" name="Map" value="<?php echo $_GET['Map']; ?>">
     <input class="btn btn-outline-primary btn-block" type="submit" name="unassign" value="Hand Map In">
