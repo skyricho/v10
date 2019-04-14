ce8a046be42d4c4394673717405a1607a846e172
Sky Richardson <skyricho@gmail.com>
2019-04-14 16:07:39 +1000

    Add pag eload time

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+$time = microtime();
+$time = explode(' ', $time);
+$time = $time[1] + $time[0];
+$start = $time;
+
 include ("dbaccess.php"); 
 require 'vendor/autoload.php';
 //ini_set('display_errors', 1);
@@ -129,5 +134,11 @@ if (isset($_GET['Block'])) {
 
 }
 
+$time = microtime();
+$time = explode(' ', $time);
+$time = $time[1] + $time[0];
+$finish = $time;
+$total_time = round(($finish - $start), 4);
+echo 'Page generated in '.$total_time.' seconds.';
 
 ?>
