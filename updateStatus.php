37c966186c18bbd6ef6b2ea72f71a220d8cac1b0
Sky Richardson <skyricho@gmail.com>
2020-06-16 10:35:48 +1000

    Add 'letter sent' label

--- updateStatus.php
+++ updateStatus.php
@@ -13,21 +13,28 @@ if (isset($_POST['id'])) {
             <input type="hidden" name="streetNumber" value="' . $_POST['streetNumber'] . '">
             <input type="hidden" name="streetName" value="' . $_POST['streetName'] . '">
             <input type="hidden" name="addressDescription" value="' . $_POST['addressDescription'] . '">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="{{ address.recID }}-checkbox" ic-post-to="updateStatus.php" ic-target="#' . $_POST['id'] . '" ic-confirm="Are you sure?" checked>
-              <label class="form-check-label text-muted" for="defaultCheck1">' . 
-                $_POST['streetNumber'] . ' ' . $_POST['streetName'];
+            <div class="d-flex justify-content-between text-muted">
+              <div>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="{{ address.recID }}-checkbox" ic-post-to="updateStatus.php" ic-target="#' . $_POST['id'] . '" ic-confirm="Are you sure?" checked>
+                  <label class="form-check-label text-muted" for="defaultCheck1">' . 
+                    $_POST['streetNumber'] . ' ' . $_POST['streetName'] . '
+                  </label>
+                </div>
+              </div>
+              <div>
+                <i class="fas fa-mail-bulk"></i> Letter sent
+              </div>
+            </div>';
                 # Hide visited date
                 //<span id="' . $_POST['id'] . '-visitDate" class="ml-3 badge-pill badge-light"><small><i>' . date("D j M") . '</i></small></span>
     if (empty($_POST['addressDescription'])) {
       echo '';
     } else {
-      echo '</br><small class="text-muted">' . $_POST['addressDescription'] . '</small>';
+      echo '<div><small class="text-muted">' . $_POST['addressDescription'] . '</small></div>';
     }
-    echo     '</label>
-            </div>
-          </form>';
-  } else {
+    echo '</form>';
+    } else {
     $edit->setField('isHome', '');
     # Not home form
     echo '<form id="' . $_POST['id'] . '-form">
