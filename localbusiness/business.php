c833da8b76a9d22a80a15cf5a7b91df19928e35d
Sky Richardson <skyricho@gmail.com>
2020-09-19 15:25:20 +1000

    update buisness info

---
+++ localbusiness/business.php
@@ -0,0 +1,22 @@
+<?php
+include ("../dbaccess.php");
+ini_set('display_errors', 1);
+
+} elseif (isset($_POST['update'])) {
+    $edit = $fm->newEditCommand('LocalBusiness', $_POST['id']);
+    $edit->setField('assignedTo', $_POST['assignedTo']);
+    $edit->setField('map', $_POST['map']);
+    $edit->setField('block', $_POST['block']);
+    $edit->setField('businessName', $_POST['businessName']);
+    $edit->setField('streetAddressRaw', $_POST['address']);
+    $edit->setField('contactName', $_POST['contactName']);
+    $edit->setField('contactPosition', $_POST['contactPosition']);
+    $edit->setField('contactEmail', $_POST['contactEmail']);
+    $edit->setField('FormSubmissionUser', $_POST['user']);
+    $edit->execute();
+
+    echo 'Saved.';
+}
+
+
+?>
