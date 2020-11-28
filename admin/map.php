f0a215fd24d4b1c984b31f0e2c6995a71883e05a
Sky Richardson <skyricho@gmail.com>
2020-11-28 15:25:03 +1100

    Update assignees.json on assign map

--- admin/map.php
+++ admin/map.php
@@ -118,15 +118,72 @@ if (FileMaker::isError($result)) {
 	$edit = $fm->newEditCommand('Map', $_POST['id']);
 	$edit->setField('MapAssignment::map', $_POST['id']);
     $edit->setField('MapAssignment::userId', $_POST['userId']);
+    $edit->setField('userID', $_POST['userId']);
+    $edit->setField('coverageType', $_POST['coverageType']);
 	$edit->setField('MapAssignment::handOutDate', date("m/d/Y"));
     $edit->setField('MapAssignment::coverageType', $_POST['coverageType']);
+    $edit->setField('coverageType', $_POST['coverageType']);
     $edit->setField('MapAssignment::startContacted', $_POST['cAH']);
 	$edit->execute();
 
-		//TODO
-	//send Map ID in form data, send userID as well, coveragetype as weell
-	// update Map with userid and coverageType
+    	$request = $fm->newFindCommand('MapAssignees');
+	$request->addFindCriterion('MapCount', '>0');
+	$request->addSortRule('First' ,1,FILEMAKER_SORT_ASCEND);
+	$result = $request->execute();
 
+	# Trap for errors
+	if (FileMaker::isError($result)) {
+	    $errorMessage = $result->getMessage();
+	    echo $errorMessage;
+	} else {
+	    /*$records = $result->getRecords();
+
+	    $availableMaps = array();
+	    foreach($records as $record) {
+	        $availableMaps[] = array(
+	            'firstName' => $record->getField('First'),
+	            //'Suburb' => $record->getField('MapSuburb::suburb'),
+	            //'Colour' => $record->getField('Suburb::badgeColour'),
+	            //'Name' => $record->getField('MapAssignment::cFirstName'),
+	            //'coverageType' => $record->getField('MapAssignment::coverageType'),
+
+	            //'toContact' => $record->getField('nhTotal'),
+	        );
+	    }
+
+
+	    print_r($availableMaps);
+
+	    echo '<br><hr><br>';*/
+
+	    $records = $result->getRecords();
+	    $var = array();
+	    foreach($records as $record) {
+	        $related_records = $record->getRelatedSet('Map');
+	        $var1 = array();
+	        foreach($related_records as $related_record) {
+	            $var1[] = array(
+	            'mapNumber' => $related_record->getField('Map::Map'),
+	            'coverageType' => $related_record->getField('Map::coverageType'),
+	            'suburb' => $related_record->getField('Map::cSuburb'),
+	            'badgeColour' => $related_record->getField('Map::cBadgeColour')
+	            );
+	        }
+
+	        $var[] = array(
+	            'firstName' => $record->getField('First'),
+	            //'status' => $record->getField('status'),
+	            'Maps' => $var1
+	        );
+
+	    }
+
+	    //print_r($var);
+	    //echo json_encode($var);
+	    //Save the JSON string to a text file.
+	    file_put_contents('../assignees.json', json_encode($var));
+
+	}
 
 	$record = $fm->getRecordByID('Map', $_POST['id']);
 	echo '<div class="alert alert-success alert-dismissible fade show" role="alert" id="alert-foo">
