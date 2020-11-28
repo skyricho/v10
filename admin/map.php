c863d0e618cc7843a5b6ecda1695782485552107
Sky Richardson <skyricho@gmail.com>
2020-11-28 11:17:07 +1100

    Unassign updates assignees.json

--- admin/map.php
+++ admin/map.php
@@ -36,9 +36,75 @@ if (isset($_POST['deactivate'])) {
     $edit->setField('Map::mapAssignmentId', '');
 	$edit->execute();
 
-	//TODO
-	//send Map ID in form data, send userID as well
-	//delete userID from map, delete coverageType
+	$edit1 = $fm->newEditCommand('Map', $_POST['Map']);
+	$edit1->setField('userID', '');
+    $edit1->setField('coverageType', '');
+	$edit1->execute();
+
+	//$output = shell_exec('php ../map-assignees.php');
+	//echo "<pre>$output</pre>";
+	/* output
+	hello world!
+	*/
+
+	$request = $fm->newFindCommand('MapAssignees');
+$request->addFindCriterion('MapCount', '>0');
+$request->addSortRule('First' ,1,FILEMAKER_SORT_ASCEND);
+$result = $request->execute();
+
+# Trap for errors
+if (FileMaker::isError($result)) {
+    $errorMessage = $result->getMessage();
+    echo $errorMessage;
+} else {
+    /*$records = $result->getRecords();
+
+    $availableMaps = array();
+    foreach($records as $record) {
+        $availableMaps[] = array(
+            'firstName' => $record->getField('First'),
+            //'Suburb' => $record->getField('MapSuburb::suburb'),
+            //'Colour' => $record->getField('Suburb::badgeColour'),
+            //'Name' => $record->getField('MapAssignment::cFirstName'),
+            //'coverageType' => $record->getField('MapAssignment::coverageType'),
+
+            //'toContact' => $record->getField('nhTotal'),
+        );
+    }
+
+
+    print_r($availableMaps);
+
+    echo '<br><hr><br>';*/
+
+    $records = $result->getRecords();
+    $var = array();
+    foreach($records as $record) {
+        $related_records = $record->getRelatedSet('Map');
+        $var1 = array();
+        foreach($related_records as $related_record) {
+            $var1[] = array(
+            'mapNumber' => $related_record->getField('Map::Map'),
+            'coverageType' => $related_record->getField('Map::coverageType'),
+            'suburb' => $related_record->getField('Map::cSuburb'),
+            'badgeColour' => $related_record->getField('Map::cBadgeColour')
+            );
+        }
+
+        $var[] = array(
+            'firstName' => $record->getField('First'),
+            //'status' => $record->getField('status'),
+            'Maps' => $var1
+        );
+
+    }
+
+    //print_r($var);
+    //echo json_encode($var);
+    //Save the JSON string to a text file.
+    file_put_contents('../assignees.json', json_encode($var));
+
+}
 
 	echo '<div class="alert alert-success alert-dismissible fade show" role="alert" id="alert-foo">
 	        Map ' . $_POST['Map'] . ' has been handed in 
