b4f04d7986eef9c36851197334a981d62b8e401c
Sky Richardson <skyricho@gmail.com>
2020-12-20 22:31:04 +1100

    Rename file

---
+++ set-assignees-list.php
@@ -0,0 +1,69 @@
+<?php
+include ("dbaccess.php"); 
+ini_set('display_errors', 1);
+
+// The correct minimum set of headers that works across the most important browsers. See http://cristian.sulea.net/blog/disable-browser-caching-with-meta-html-tags/
+header('Cache-Control: no-cache, no-store, must-revalidate');
+header('Pragma: no-cache');
+header('Expires: 0');
+
+$request = $fm->newFindCommand('MapAssignees');
+$request->addFindCriterion('MapCount', '>0');
+$request->addSortRule('First' ,1,FILEMAKER_SORT_ASCEND);
+$result = $request->execute();
+
+# Trap for errors
+if (FileMaker::isError($result)) {
+    $errorMessage = $result->getMessage();
+    echo $errorMessage;
+} else {
+    /*$records = $result->getRecords();
+
+    $availableMaps = array();
+    foreach($records as $record) {
+        $availableMaps[] = array(
+            'firstName' => $record->getField('First'),
+            //'Suburb' => $record->getField('MapSuburb::suburb'),
+            //'Colour' => $record->getField('Suburb::badgeColour'),
+            //'Name' => $record->getField('MapAssignment::cFirstName'),
+            //'coverageType' => $record->getField('MapAssignment::coverageType'),
+
+            //'toContact' => $record->getField('nhTotal'),
+        );
+    }
+
+
+    print_r($availableMaps);
+
+    echo '<br><hr><br>';*/
+
+    $records = $result->getRecords();
+    $var = array();
+    foreach($records as $record) {
+        $related_records = $record->getRelatedSet('Map');
+        $var1 = array();
+        foreach($related_records as $related_record) {
+            $var1[] = array(
+            'mapNumber' => $related_record->getField('Map::Map'),
+            'coverageType' => $related_record->getField('Map::coverageType'),
+            'suburb' => $related_record->getField('Map::cSuburb'),
+            'badgeColour' => $related_record->getField('Map::cBadgeColour')
+            );
+        }
+
+        $var[] = array(
+            'firstName' => $record->getField('First'),
+            //'status' => $record->getField('status'),
+            'Maps' => $var1
+        );
+
+    }
+
+    //print_r($var);
+    echo json_encode($var);
+    //Save the JSON string to a text file.
+    file_put_contents('assignees.json', json_encode($var));
+
+}
+
+?>
