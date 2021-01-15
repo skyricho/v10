baef310e2f99a26eb192e4636d4d90d0195357e6
Sky Richardson <skyricho@gmail.com>
2021-01-15 22:05:19 +1100

    Change CDN for intercooler.js library

--- updateUnitStatus.php
+++ updateUnitStatus.php
@@ -8,17 +8,27 @@ if (isset($_POST['id'])) {
     $edit->setField('isHome', 'ah');
     # Is home form
     echo '<form id="' . $_POST['id'] . '-form">
-            <input type="hidden" name="id" value="' . $_POST['id'] . '">
+            <input type="hidden" name="id" value="' . $_POST['id']. '">
             <input type="hidden" name="notHome" value="">
             <input type="hidden" name="streetNumber" value="' . $_POST['streetNumber'] . '">
             <input type="hidden" name="housingUnitNumber" value="' . $_POST['housingUnitNumber'] . '">
-            <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="' . $_POST['id'] . '-checkbox" ic-post-to="updateUnitStatus.php" ic-target="#' . $_POST['streetNumber'] . '-unit-' . $_POST['unitID'] . '" ic-confirm="Are you sure?" checked>
-              <label class="form-check-label" for="defaultCheck1">' . 
-                'Unit ' . $_POST['housingUnitNumber'] . '
-              </label>
-            </div>
+
+            <div class="d-flex justify-content-between text-muted">
+              <div>
+                <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="' . $_POST['id'] . '-checkbox" ic-post-to="updateUnitStatus.php" ic-target="#' . $_POST['streetNumber'] . '-unit-' . $_POST['id'] . '" ic-confirm="Are you sure?" checked>
+                  <label class="form-check-label" for="defaultCheck1">' .
+                    'Unit ' . $_POST['housingUnitNumber'] . '
+                  </label>
+                </div>
+              </div>
+              <div>
+                <i class="fas fa-mail-bulk"></i> Letter sent
+              </div>
+            </div>     
           </form>';
+
+          
   } else {
     $edit->setField('isHome', '');
     # Not home form
@@ -28,7 +38,7 @@ if (isset($_POST['id'])) {
             <input type="hidden" name="streetNumber" value="' . $_POST['streetNumber'] . '">
             <input type="hidden" name="housingUnitNumber" value="' . $_POST['housingUnitNumber'] . '">
             <div class="form-check">
-              <input class="form-check-input" type="checkbox" value="" id="' . $_POST['id'] . '-checkbox" ic-post-to="updateUnitStatus.php" ic-target="#' . $_POST['streetNumber'] . '-unit-' . $_POST['unitID'] . '">
+              <input class="form-check-input" type="checkbox" value="" id="' . $_POST['id'] . '-checkbox" ic-post-to="updateUnitStatus.php" ic-target="#' . $_POST['streetNumber'] . '-unit-' . $_POST['id'] . '">
               <label class="form-check-label" for="defaultCheck1">' . 
                 'Unit ' . $_POST['housingUnitNumber'] . '
               </label>
