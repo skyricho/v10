b76875c7a809969425b1995c69898d325a4e5d4c
Sky Richardson <skyricho@gmail.com>
2021-06-19 00:46:36 +1000

    Create group-list.php

---
+++ group-list.php
@@ -0,0 +1,43 @@
+<?php
+include ("dbaccess.php"); 
+require 'vendor/autoload.php';
+ini_set('display_errors', 1);
+
+$loader = new Twig_Loader_Filesystem('views');
+$twig = new Twig_Environment($loader);
+$template = $twig->load('group-list.html.twig');
+
+
+# Query assignee list
+$request = $fm->newFindCommand('group-publishers');
+$request->addFindCriterion('overseer', $_GET['overseer']); 
+//$request->addSortRule('name', 1);
+$result = $request->execute();
+
+//$assignees = 'baz';
+# Trap for errors
+if (FileMaker::isError($result)) {
+    $errorMessage = $result->getMessage();
+} else {
+    $records = $result->getRecords();
+
+    $assignees = array();
+    foreach($records as $record) {
+        $assignees[] = array(
+            'name' => $record->getField('name'),
+        );
+    }
+    /*foreach ($records as $record) {
+        echo $record->getField('name') . '<br>';
+    }*/
+}
+
+# prepare variables for template
+echo $template->render(array(
+        'assignees' => $assignees,
+        'errorMessage' => $errorMessage
+    )
+);
+//print_r($assignees);
+//echo 'foo';
+?>
